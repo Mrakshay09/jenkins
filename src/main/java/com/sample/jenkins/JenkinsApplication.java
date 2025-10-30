56d2b31194c19a200165cccaca9881d5a947d3fb
AKSHAY SETHIYA <80109075+Mrakshay09@users.noreply.github.com>
2025-10-30 16:26:43 +0530

    Update build trigger pipeline message

--- src/main/java/com/sample/jenkins/JenkinsApplication.java
+++ src/main/java/com/sample/jenkins/JenkinsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JenkinsApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Testing Build Trigger Pipeline.....!");
+		System.out.println("Testing Build Trigger Pipeline.....!v222222");
 		SpringApplication.run(JenkinsApplication.class, args);
 	}
 
